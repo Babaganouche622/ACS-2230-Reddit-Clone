58a73cb320a8039abeaf503a79025231743bc5ca
Brian <briangordon.cahill@students.dominican.edu>
2023-04-22 14:47:44 -0700

    Added login log out

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,4 +18,37 @@ module.exports = (app) => {
       return res.status(400).send({ err: err });
     }
   });
+
+  // LOGOUT
+  app.get('/logout', (req, res) => {
+    res.clearCookie('nToken');
+    res.redirect('/');
+  });
+
+  // LOGIN FORM
+  app.get('/login', (req, res) => res.render('login'));
+
+  // LOGIN
+  app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      // User not found
+      return res.status(401).send({ message: 'Wrong Username or Password' });
+    }
+    user.comparePassword(password, (err, isMatch) => {
+      if (!isMatch) {
+        // Password does not match
+        return res.status(401).send({ message: 'Wrong Username or password' });
+      }
+      // Create a token
+      const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+        expiresIn: '60 days',
+      });
+      // Set a cookie and redirect to root
+      res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+      res.redirect('/');
+    }
+    );
+  });
 };
